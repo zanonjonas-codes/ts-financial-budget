a16eca75d5086e7ebaf78446ac14042b3ab0f1cd
Jonas Zanon <zanonjonas.codes@gmail.com>
2024-01-14 00:23:38 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="grid grid-cols-1 gap-2 p-3 justify-items-center">
+        <div>
+          <input className="rounded-md h-10" />
+        </div>
+        <div>
+          <input className="rounded-md h-10" />
+        </div>
+        <div>
+          <button className="btn btn-primary rounded-md h-10">Login</button>
+        </div>
+      </div>
+    </main>
+  )
+}
---
+++ src/pages/login.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import '../app/globals.css'
+
+export interface ILoginProps {}
+
+export function Login(props: ILoginProps) {
+  return <section className="h-screen flex flex-col"></section>
+}
+
+export default Login
