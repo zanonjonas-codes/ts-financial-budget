ef854fcdd91d7e0f604c399937f76edd62a1c48b
Jonas Zanon <zanonjonas.codes@gmail.com>
2024-01-14 00:29:53 -0300

    initial commit

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,7 +4,11 @@ import '../app/globals.css'
 export interface ILoginProps {}
 
 export function Login(props: ILoginProps) {
-  return <section className="h-screen flex flex-col"></section>
+  return (
+    <section className="h-screen flex flex-col">
+      <div className=""></div>
+    </section>
+  )
 }
 
 export default Login
